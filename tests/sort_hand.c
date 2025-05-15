5fa04c84d5ccee06b0cb64b8ad7c106afc3e4d9c
andy5995 <arch_stanton5995@proton.me>
2025-05-15 18:35:32 -0500

    Add missing test file

---
+++ tests/sort_hand.c
@@ -0,0 +1,22 @@
+#include "00_test.h"
+
+_MAIN_HEAD_
+
+struct pokeval_hand_t hand = {.card = {
+                                  {TEN, HEARTS},
+                                  {THREE, CLUBS},
+                                  {ACE, DIAMONDS},
+                                  {FIVE, SPADES},
+                                  {KING, CLUBS},
+                              }};
+
+sort_hand(&hand);
+
+int expected[] = {ACE, KING, TEN, FIVE, THREE};
+for (int i = 0; i < HAND_SIZE; ++i) {
+  fprintf(stderr, "card: %d | ", hand.card[i].face_val);
+  assert(hand.card[i].face_val == expected[i]);
+}
+putchar('\n');
+
+_MAIN_TAIL_
